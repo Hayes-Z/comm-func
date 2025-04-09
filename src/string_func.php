73aad3cb5b2f1aab1d234b6c8b75020c16957d8e
944631794@qq.com <944631794@qq.com>
2025-04-09 16:49:26 +0800

    字符函数

---
+++ src/string_func.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace HayesZ\CommonFunc;
+
+/**
+ * 生成随机字符串
+ * LC 小写字母 UC 大写字母 NUM 数字 SYM 字符
+ * @param $length
+ * @param $rules
+ * @return string
+ * @throws \Exception
+ */
+function randomString($length,$rules=['LC','UC','NUM']){
+    $rules = array_unique($rules);
+    $str_map = array(
+        'UC' => "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        'LC' => "abcdefghijklmnopqrstuvwxyz",
+        'NUM' => "0123456789",
+        'SYM' => "!@#$%^&*?_~",
+    );
+    $charactersArr = [];
+    foreach ($rules as $r){
+        $charactersArr[] = $str_map[$r];
+    }
+    $characters = implode('',$charactersArr);
+    $charLength = strlen($characters);
+
+    $bytes = random_bytes($length);
+    $returnString = '';
+    for ($i = 0; $i < $length; ++$i) {
+        // 直接映射 0-255 到 字符
+        $returnString .= $characters[ord($bytes[$i]) & $charLength];
+    }
+    return $returnString;
+}
